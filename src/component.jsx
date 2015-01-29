1e2662ec232e013d27733de85ca0b64759e2decb
MIke Carter <mike@mcarter.me>
2015-01-29 18:56:26 +0000

    Fix broken stuff.

--- src/component.jsx
+++ src/component.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var HelloWorld = React.createClass({
   render: function() {
     // There might be a nicer way of resolving these at build time...
-    return require(process.env.RELATIVE_TEMPLATE_PATH); T
+    return require(process.env.RELATIVE_TEMPLATE_PATH);
   }
 });
 
