c3a0444bbb558d5f9303f0fe17f44e189a5dee32
MIke Carter <mike@mcarter.me>
2015-01-29 18:42:23 +0000

    Fix broken stuff.

--- scripts/build-dev.sh
+++ scripts/build-dev.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
-if [ -z "$TEMPLATE_VARIANT" ]; then TEMPLATE_VARIANT='default'; fi
+if [ -z "$VARIANT" ]; then VARIANT='default'; fi
 if [ -z "$TEMPLATE_LANG" ]; then TEMPLATE_LANG='en'; fi
 
-echo "Building package for demo. Variant: '$TEMPLATE_VARIANT', language: '$TEMPLATE_LANG'."
+echo "Building package for demo. Variant: '$VARIANT', language: '$TEMPLATE_LANG'."
 
-RELATIVE_TEMPLATE_PATH="./templates/$TEMPLATE_VARIANT.jsx" browserify src/component.jsx -o dev/component.js
+RELATIVE_TEMPLATE_PATH="./templates/$VARIANT.jsx" browserify src/component.jsx -o dev/component.js
